f0290ec0e799b4eb80a1b6c1473e467fe8a8ae7e
viethoang5426 <caohoang5969@gmail.com>
2024-11-28 22:44:37 +0700

    commit 44

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello from Node js");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello form node js");
+});
+
+app
+  .listen(port, () => {
+    console.log(`Server listening
+ on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error starting server:", err);
+  });
